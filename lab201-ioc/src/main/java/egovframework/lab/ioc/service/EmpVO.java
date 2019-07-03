4aeadd3e276db2d5045a065eee76b280168d7547
lx9203 <lx9203@naver.com>
2019-07-03 10:28:47 +0900

    Initial commit

---
+++ lab201-ioc/src/main/java/egovframework/lab/ioc/service/EmpVO.java
@@ -0,0 +1,40 @@
+	package egovframework.lab.ioc.service;
+
+import java.io.Serializable;
+
+public class EmpVO implements Serializable, Comparable<EmpVO> {
+	private int empNo;
+	private String empName;
+	private String job;
+    // TODO [Step 1-2, 2-2] EmpVO 작성
+	
+	public int getEmpNo() {
+		return empNo;
+	}
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	
+	public int compareTo(EmpVO o){
+		if(empNo > o.getEmpNo()){
+			return 1;
+		} else if (empNo < o.getEmpNo()){
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+
+}
