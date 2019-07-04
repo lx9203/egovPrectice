315df99b3a11e600ec4b81521f4cbf8aa2fc27c0
lx9203 <lx9203@naver.com>
2019-07-04 17:03:21 +0900

    lab202

--- lab202-aop/src/main/java/egovframework/lab/aop/xml/AdviceUsingXML.java
+++ lab202-aop/src/main/java/egovframework/lab/aop/xml/AdviceUsingXML.java
@@ -3,23 +3,86 @@ package egovframework.lab.aop.xml;
 import java.util.List;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.stereotype.Component;
 
 import egovframework.lab.aop.common.BizException;
 
 public class AdviceUsingXML {
-
-	//TODO [Step 1-6] AdviceUsingXML 작성
+	private final Log sysoutLogger = LogFactory.getLog(AdviceUsingXML.class);
+	
+	public void beforeTargetMethod(JoinPoint thisJoinPoint) {
+		sysoutLogger.debug("\nAdviceUsingXML.beforeTargetMethod executed.");
+		
+		@SuppressWarnings("unchecked")
+		Class clazz = thisJoinPoint.getTarget().getClass();
+		String className = thisJoinPoint.getSignature().getName();
+		String methodName = thisJoinPoint.getSignature().getName();
+		
+		StringBuffer buf = new StringBuffer();
+		buf.append("\n == AdviceUsingXML.beforeTargetMethod : [" + className + "." + methodName + "()] ==");
+		Object[] arguments = thisJoinPoint.getArgs();
+		int argCount = 0;
+		for(Object obj : arguments){
+			buf.append("\n = arg");
+			buf.append(argCount++);
+			buf.append(":");
+			buf.append(ToStringBuilder.reflectionToString(obj));
+		}
+		
+		Log logger = LogFactory.getLog(clazz);
+		if(logger.isDebugEnabled())
+			logger.debug(buf.toString());
+	}
+	
+	public void afterTargerMethod(JoinPoint thisJoinPoint){
+		sysoutLogger.debug("AdviceUsingXML.afterTargetMetgod excuted.");
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void afterReturningTargetMethod(JoinPoint thisJoinPoint, Object retVal) {
+		sysoutLogger.debug("AdviceUsingXML.afterReturningTargetMetgod excuted.");
+		
+		Class clazz = thisJoinPoint.getTarget().getClass();
+		String className = thisJoinPoint.getTarget().getClass().getSimpleName();
+		String methodName = thisJoinPoint.getSignature().getName();
+		
+		StringBuffer buf = new StringBuffer();
+		buf.append("\n == AdviceusingXML.afterReturningTargetMethod : [" + className + "." + methodName + "()] ==");
+		buf.append("\n");
+		
+		if(retVal instanceof List) {
+			List resultList = (List) retVal;
+			buf.append("resultList size : " + resultList.size() + "\n");
+			for ( Object oneRow : resultList) { 
+				buf.append(ToStringBuilder.reflectionToString(oneRow));
+				buf.append("\n");
+			}
+		} else {
+			buf.append(ToStringBuilder.reflectionToString(retVal));
+		}
+		
+		Log logger = LogFactory.getLog(clazz);
+		if(logger.isDebugEnabled())
+			logger.debug(buf.toString());
+	}
+	
+	public void afterThrowingTargetMethod(JoinPoint thisJoinPoint, Exception exception) throws Exception {
+		sysoutLogger.debug("AdviceusingXML.afterThrowingTargetMethod executed");
+		sysoutLogger.error("에러가 발생햇습니다.", exception);
+		
+		throw new BizException("에러가 발생했습니다.", exception);
+	}
+	
+	public Object aroundTargetMethod(ProceedingJoinPoint thisJoinPoint) throws Throwable {
+		sysoutLogger.debug("AdviceusingXML.aroundTargetMethod start.");
+		long time1 = System.currentTimeMillis();
+		Object retVal = thisJoinPoint.proceed();
+		long time2 = System.currentTimeMillis();
+		sysoutLogger.debug("AdviceusingXML.aroundTargetMethod end. Time(" + (time2 - time1) + ")");
+		return retVal;
+	}
 	
 }
--- lab202-aop/src/test/java/egovframework/lab/aop/service/EmpServiceTest.java
+++ lab202-aop/src/test/java/egovframework/lab/aop/service/EmpServiceTest.java
@@ -1,9 +1,6 @@
 package egovframework.lab.aop.service;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
@@ -17,12 +14,86 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import egovframework.lab.aop.common.BizException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {
-	"classpath*:META-INF/spring/context-common.xml",
-	"classpath*:META-INF/spring/context-emp.xml",
-	"classpath*:META-INF/spring/context-advice-xml.xml"})
+@ContextConfiguration(locations = { "classpath*:META-INF/spring/context-common.xml",
+		"classpath*:META-INF/spring/context-emp.xml", "classpath*:META-INF/spring/context-aspectj-annotation.xml",
+		"classpath*:META-INF/spring/context-advice-xml.xml" })
 public class EmpServiceTest {
 
 	// TODO [Step 1-8, 2-8] EmpServiceTest 작성
+	@Resource(name = "xmlEmpService")
+	EmpService empService;
 
+	public EmpVO makeVO() {
+		return makeVO(101);
+	}
+
+	public EmpVO makeVO(int empNo) {
+		EmpVO vo = new EmpVO();
+		vo.setEmpNo(empNo);
+		vo.setEmpName("홍길동" + empNo);
+		vo.setJob("개발자");
+		return vo;
+	}
+
+	public void checkResult(EmpVO vo, EmpVO resultVO) {
+		assertNotNull(resultVO);
+		assertEquals(vo.getEmpNo(), resultVO.getEmpNo());
+		assertEquals(vo.getEmpName(), resultVO.getEmpName());
+		assertEquals(vo.getJob(), resultVO.getJob());
+	}
+
+	@Test
+	public void testInsetEmp() throws Exception {
+		EmpVO vo = makeVO();
+		empService.insertEmp(vo);
+		EmpVO resultVO = empService.selectEmp(vo);
+		checkResult(vo, resultVO);
+	}
+
+	@Test
+	public void testUpdateEmp() throws Exception {
+		EmpVO vo = makeVO(102);
+		empService.insertEmp(vo);
+		vo.setEmpName("홍길순");
+		vo.setJob("설계자");
+		empService.updateEmp(vo);
+		EmpVO resultVO = empService.selectEmp(vo);
+		checkResult(vo, resultVO);
+	}
+
+	@Test
+	public void testDeleteEmp() throws Exception {
+		EmpVO vo = makeVO(103);
+		empService.insertEmp(vo);
+		empService.deleteEmp(vo);
+		try {
+			@SuppressWarnings("unused")
+			EmpVO resultVO = empService.selectEmp(vo);
+			fail("Biz Excpetion 이 발생해야 합니다.");
+
+		} catch (Exception e) {
+			assertNotNull(e);
+			assertTrue(e instanceof BizException);
+			assertEquals("에러가 발생했습니다.", e.getMessage());
+			assertEquals("no data found!", e.getCause().getMessage());
+		}
+
+	}
+	
+	@Test
+	public void testSelectEmpList() throws Exception {
+		List<EmpVO> resultList = empService.selectEmpList();
+		int firstListSize = resultList.size();
+		assertEquals(1, resultList.get(0).getEmpNo());
+		EmpVO empVO = new EmpVO();
+		empVO.setEmpNo(1);
+		
+		empService.deleteEmp(empVO);
+		resultList = empService.selectEmpList();
+		assertEquals(firstListSize -1, resultList.size());
+		assertEquals(2, resultList.get(0).getEmpNo());
+		assertEquals("EmpName 2", resultList.get(0).getEmpName());
+		assertEquals("SALESMAN", resultList.get(0).getJob());
+		
+	}
 }
